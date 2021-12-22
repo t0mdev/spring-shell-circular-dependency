457da6be1f2ae443d8221d885ddf2d1af241cacd
Thomas Kiendl <thomas.kiendl@tngtech.com>
2021-12-22 09:54:24 +0100

    Spring initializer for empty spring project.

---
+++ src/test/java/com/example/springshellcirculardependency/SpringShellCircularDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springshellcirculardependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellCircularDependencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
